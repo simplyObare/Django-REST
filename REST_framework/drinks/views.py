1176e7c60fb2ba032cc405b02442ea4152242ac7
simplyObare <josephatowiny@gmail.com>
2025-01-25 04:51:47 +0300

    Django REST Framework

---
+++ REST_framework/drinks/views.py
@@ -0,0 +1,45 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+from .models import Drink
+from .serializers import DrinkSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(["GET", "POST"])
+def drink_list(request):
+    if request.method == "GET":
+        drinks = Drink.objects.all()
+        serializer = DrinkSerializer(drinks, many=True)
+        return Response(serializer.data)
+
+    if request.method == "POST":
+        serializer = DrinkSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def drink_detail(request, id):
+
+    try:
+        drink = Drink.objects.get(pk=id)
+    except Drink.DoesNotExist:
+        return Response(status=404)
+
+    if request.method == "GET":
+        serializer = DrinkSerializer(drink)
+        return Response(serializer.data)
+
+    elif request.method == "PUT":
+        serializer = DrinkSerializer(drink, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+
+    elif request.method == "DELETE":
+        drink.delete()
+        return Response(status=204)

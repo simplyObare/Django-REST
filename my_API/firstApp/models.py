986f5a0d52ca84fc5e0c306d8f6d53074cc41226
simplyObare <josephatowiny@gmail.com>
2025-01-26 07:40:42 +0300

    create model class

--- my_API/firstApp/models.py
+++ my_API/firstApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class CarSpecs(models.Model):
+    car_brand = models.CharField(max_length=100)
+    car_model = models.CharField(max_length=100)
+    production_year = models.IntegerField()
+    car_body = models.CharField(max_length=100)
+    engine_type = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Car brand: {self.car_brand} Car Model: {self.car_model} Year: {self.production_year}"

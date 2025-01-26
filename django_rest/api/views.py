7011e42935c2a294569d3fcf89db3d0a69d3a3ed
simplyObare <josephatowiny@gmail.com>
2025-01-27 01:57:51 +0300

    Python Django REST API

---
+++ django_rest/api/views.py
@@ -0,0 +1,21 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import User
+from .serializer import UserSerializer
+from rest_framework import status
+
+
+@api_view(["GET"])
+def get_user(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(["POST"])
+def create_user(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

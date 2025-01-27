7a0a1adb540f1c50539c09e2556de207d12c950d
simplyObare <josephatowiny@gmail.com>
2025-01-28 01:25:11 +0300

    setting up the front-end

---
+++ django-react/client/app/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Book Website</h1>
+
+      <div className="input-section">
+        <input type="text" name="" id="" placeholder="Book Title" />
+        <input type="date" name="" id="" placeholder="Published Date" />
+        <button>Add Book</button>
+      </div>
+    </>
+  )
+}
+
+export default App

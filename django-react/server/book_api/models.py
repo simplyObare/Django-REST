813a623ba5e8c9a359f99b70cacc02595c2359c8
simplyObare <josephatowiny@gmail.com>
2025-01-28 01:58:11 +0300

    create models

--- django-react/server/book_api/models.py
+++ django-react/server/book_api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Book(models.Model):
+    book_title = models.CharField(max_length=255)
+    date_published = models.DateField()
+
+    def __str__(self):
+        return self.book_title

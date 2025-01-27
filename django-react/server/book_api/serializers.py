9b44ecc9f92e8055866cc29bf8f392e5d3df07db
simplyObare <josephatowiny@gmail.com>
2025-01-28 02:39:00 +0300

    create serializers, urls and views

---
+++ django-react/server/book_api/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Book
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = "__all__"
+
+    def create(self, validated_data):
+        return Book.objects.create(**validated_data)

dca56f0e38d123e724eb683a1201ea66e10376fb
simplyObare <josephatowiny@gmail.com>
2025-01-28 04:13:51 +0300

    connecting the frontend to the backend

--- django-react/client/app/src/App.jsx
+++ django-react/client/app/src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 
 function App() {
   const [books, setBooks] = useState([])
+  const [bookTitle, setBookTitle] = useState('')
+  const [datePublished, setDatePublished] = useState('')
 
   useEffect(() => {
     fetchBooks()
@@ -18,14 +20,48 @@ function App() {
     }
   }
 
+  const addBook = async () => {
+    const bookData = {
+      book_title: bookTitle,
+      date_published: datePublished,
+    }
+
+    try {
+      const response = await fetch('http://127.0.0.1:8000/api/books/create/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(bookData),
+      })
+
+      const data = await response.json()
+      setBooks((prev) => [...prev, data])
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <>
       <h1>Book Website</h1>
 
       <div className="input-section">
-        <input type="text" name="" id="" placeholder="Book Title" />
-        <input type="date" name="" id="" placeholder="Published Date" />
-        <button>Add Book</button>
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="Book Title"
+          onChange={(e) => setBookTitle(e.target.value)}
+        />
+        <input
+          type="date"
+          name=""
+          id=""
+          placeholder="Published Date"
+          onChange={(e) => setDatePublished(e.target.value)}
+        />
+        <button onClick={addBook}>Add Book</button>
       </div>
 
       <div className="books-section">

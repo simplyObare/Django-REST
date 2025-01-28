25ebf1a41e9d3caeedeff1001558890748fa9abd
simplyObare <josephatowiny@gmail.com>
2025-01-28 03:32:59 +0300

    connecting the backend with the frontend

--- django-react/client/app/src/App.jsx
+++ django-react/client/app/src/App.jsx
@@ -1,6 +1,23 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    fetchBooks()
+  }, [])
+
+  const fetchBooks = async () => {
+    try {
+      const response = await fetch('http://127.0.0.1:8000/api/books/')
+      const data = await response.json()
+      setBooks(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <>
       <h1>Book Website</h1>
@@ -9,6 +26,15 @@ function App() {
         <input type="text" name="" id="" placeholder="Book Title" />
         <input type="date" name="" id="" placeholder="Published Date" />
         <button>Add Book</button>
+      </div>
+
+      <div className="books-section">
+        {books.map((book) => (
+          <div className="book" key={book.id}>
+            <h2>Title: {book.book_title}</h2>
+            <p>Published: {book.date_published}</p>
+          </div>
+        ))}
       </div>
     </>
   )
